08f1762a696d92578ca95523cffb8e5481423c6b
nilesh <savaliyanilesh08@gmail.com>
2022-10-13 18:19:40 +0530

    first commit

---
+++ src/Data/Data.js
@@ -0,0 +1,114 @@
+const Data = [
+  {
+    mainPage: {
+      home: {
+        title: "Home",
+        name: "Nilesh Savaliya",
+        about:
+          "Web developer intern, Flair for creating impressive visuals, delivering web identity to the business",
+        socialMedia: {
+          facebook: "https://facebook.com/nilsavaliya8539",
+          instagram: "https://www.instagram.com/_neelsavaliya?",
+          linkedin: "http://www.linkedin.com/in/nileshsavaliya",
+        },
+        profilePic:
+          "https://cdn.pixabay.com/photo/2017/10/14/10/38/man-2850159_960_720.jpg",
+      },
+
+      resume: {
+        title: "Resume",
+        objective: {
+          title: "CAREER OBJECTIVE",
+          description:
+            "To secure a challenging position in a reputable organization to expand my learning, knowledge, and skills. secure a responsible career opportunity to fully utilize my training and skills, while making a significant contribution to the success of the company. Seeking an entry-level position to begin my career in a high- level professional environment. Possesses verbal communication skills and excellent interpersonal skills.",
+        },
+        experience: {
+          title: "EXPERIENCE",
+          present: {
+            title: "ISMT (Jejuri, Pune)",
+            list: {
+              i: "Lab testing of material.",
+              ii: " Perform Mechanical and Metallurgical testing regularly And Initiate Appropriate Corrective Actions on quality issues",
+              iii: "Prepare actual test certificate of steel grade.",
+              iv: "Support the Quality Department on customer problem.",
+            },
+            duration: "DEC 2016 - MAY 2022",
+          },
+
+          past: {
+            title: "UTTAM VALUE STEEL Ltd.(Wardha)",
+            list: {
+              i: " Melting of steel and make grade as per customer requirements as per standard",
+              ii: " Organized And Analyzed Production Statistics To Ensure That The Furnace Was Being Operated Efficiently And Gained Insights Improvements.",
+              iii: "Prepare weekly energy chart and oxygen consumption chart of furnace and try to reduce extra consumption for cost saving.",
+              iv: "Collaborated With Management Staff To Overcome Challenges Related To Environmental, Safety, Production And Economics.",
+            },
+            duration: "AUG 2015 - DEC 2016",
+          },
+        },
+
+        education: {
+          title: "EDUCATION",
+          degree: {
+            name: "BACHELOR IN METALLURGICAL ENGINEERING (Part-Time)",
+            college:
+              "From Mangalam School Of Management And Technology with first division",
+            year: "2015 - 2018",
+          },
+          diploma: {
+            name: "DIPLOMA METALLURGY ENGINEERING",
+            college: "From Gujarat Technological University with CGPA of 8.51",
+            year: "2012 - 2015",
+          },
+          ssc: {
+            title: "SSC",
+            board: "From GUJARAT BOARD with Percentage of 78.48",
+            year: "2012",
+          },
+        },
+        skill: {
+          title: "SKILL SET",
+          skills: "HTML5, CSS, JavaScript, Git-Hub, Netlify, Adobe-Photoshop",
+        },
+      },
+
+      about: {
+        title: "About",
+        brief:
+          "I'm Nilesh, I am a front-end developer intern. I am experienced in steel making and quality control of steel but I am passionate about creating user-friendly and responsive websites.",
+      },
+
+      project: {
+        title: "Projects",
+        project1: {
+          title: "Website Layout",
+          link: "https://trellonileshfsd.netlify.app/",
+        },
+        project2: {
+          title: "To-do App",
+          link: "https://tranquil-tarsier-902ebc.netlify.app/",
+        },
+        project3: {
+          title: "Login-Page",
+          link: "https://profound-gelato-08ff21.netlify.app/",
+        },
+      },
+
+      contact: {
+        title: "CONTACT",
+        email: {
+          title: "savaliyanilesh08@gmail.com",
+          link: "mail:savaliyanilesh08@gmail.com",
+        },
+        mobile: {
+          title: "+917767019917",
+          link: "tel:+917767019917",
+        },
+      },
+      // project: "Projects",
+      // contact: "contact",
+    },
+  },
+];
+
+export default Data;
